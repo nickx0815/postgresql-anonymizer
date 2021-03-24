6e7b62dcda47bb2a58f8b4326be72d3666aefcd9
Herold, Nick <n.herold@intero-technologies.de>
2021-03-24 13:09:26 +0100

    removed pypika queries

--- pganonymizer/revert.py
+++ pganonymizer/revert.py
@@ -18,20 +18,27 @@ def create_anon_db(connection, data, ids):
     _run_query(cr, data, ids)
     
 def _run_query(cr, data, ids):
-    table_ir_model = Table('ir_model')
-    table_ir_model_fields = Table('ir_model_fields_anonymization')
-    table_anon_db = Table('anon_db')
+#     table_ir_model = Table('ir_model')
+#     table_ir_model_fields = Table('ir_model_fields_anonymization')
+#     table_anon_db = Table('anon_db')
     for table in data:
-        table_sql = str(Query.from_(table_ir_model).select("id").where(table_ir_model.model == _(table)))
+        table_sql = "Select id FROM ir_model WHERE model = '{model_data}'".format(model_data=_(table))
+        #table_sql = str(Query.from_(table_ir_model).select("id").where(table_ir_model.model == _(table)))
         cr.execute(table_sql)
         table_id = cr.fetchone()[0]
         for field in data.get(table):
-            field_sql = str(Query.from_(table_ir_model_fields).select('id').where(table_ir_model_fields.field_name == field).where(table_ir_model_fields.model_id == table_id).where(table_ir_model_fields.id.isin(ids)))
+            field_sql = "Select id From ir_model_fields_anonymization Where field_name = '{field_name}' AND model_id = {table_id} and id in {tuple_ids}".format(field_name=field,
+                                                                                                                                                                table_id=table_id,
+                                                                                                                                                                ids=tuple(ids))
+            #field_sql = str(Query.from_(table_ir_model_fields).select('id').where(table_ir_model_fields.field_name == field).where(table_ir_model_fields.model_id == table_id).where(table_ir_model_fields.id.isin(ids)))
             cr.execute(field_sql)
             field_id = cr.fetchone()[0]
             try:
                 for id in data.get(table).get(field):
-                    sql_anon_db_insert = str(Query.into(table_anon_db).insert(table_id, field_id, id, data.get(table).get(field).get(id)))
+                    sql_anon_db_insert = "Insert into anon_db (model_id, field_id, record_id, value) \
+                    VALUES ({model_id}, {field_id}, {record_id}, '{value}')".format(
+                        model_id = table_id, field_id = field_id, id = id, value = data.get(table).get(field).get(id))
+                    #sql_anon_db_insert = str(Query.into(table_anon_db).insert(table_id, field_id, id, data.get(table).get(field).get(id)))
                     cr.execute(sql_anon_db_insert)
                 update_fields_history(cr, table_id, field_id)
                 cr.execute("COMMIT;")
