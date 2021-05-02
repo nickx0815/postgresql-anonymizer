19488d86d143a5d09d48fcefabbdf3d58c4a500f
Herold, Nick <n.herold@intero-technologies.de>
2021-05-02 15:43:54 +0200

    even more fixes

--- pganonymizer/revert.py
+++ pganonymizer/revert.py
@@ -32,7 +32,7 @@ def _run_query(con, data, ids):
             field_id = cr.fetchone()[0]
             for id in data.get(table).get(field):
                 sql_anon_db_insert = "Insert into anon_db (model_id, field_id, record_id, value) \
-                VALUES ({model_id}, {field_id}, {record_id}, '{value}')".format(
+                VALUES ('{model_id}', '{field_id}', {record_id}, '{value}')".format(
                     model_id = table, field_id = field, record_id = id, value = data.get(table).get(field).get(id))
                 cr.execute(sql_anon_db_insert)
                 update_fields_history(cr, table_id, field_id)
