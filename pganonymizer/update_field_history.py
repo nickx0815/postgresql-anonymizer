a59942892a5ddc0ffe86a7ca876131d535ae1c7b
Herold, Nick <n.herold@intero-technologies.de>
2021-05-02 15:49:03 +0200

    fix update_history model_id

--- pganonymizer/update_field_history.py
+++ pganonymizer/update_field_history.py
@@ -4,10 +4,11 @@ def update_fields_history(con, model_id, field_id):
     now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     con.execute("""\
         Insert into ir_model_fields_anonymization_history (
-            state, field_id, create_date, write_date, create_uid
+            state, model_id, field_id, create_date, write_date, create_uid
         ) values (
-            {state}, {field_id}, '{create_date}', '{write_date}', {create_uid});
+            {state}, {model_id},{field_id}, '{create_date}', '{write_date}', {create_uid});
         """.format(state = 2,
+                   model_id = model_id,
                    field_id = field_id,
                    create_date = str(now),
                    write_date = str(now),
