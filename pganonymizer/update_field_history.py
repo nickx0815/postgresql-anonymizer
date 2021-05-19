7ebe1bf167f7bad8a6c82e6f1e9cf61f19d22af1
Herold, Nick <n.herold@intero-technologies.de>
2021-05-19 16:28:26 +0200

    fix

--- pganonymizer/update_field_history.py
+++ pganonymizer/update_field_history.py
@@ -4,10 +4,11 @@ def update_fields_history(cr, model_id, record, state, field_id=False, revert_fi
     now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     if field_id:
         cr.execute("Insert into ir_model_fields_anonymization_history ( \
-                state, model_id, field_id, create_date, write_date, create_uid, record_id \
+                state, model_id, field_to_group, field_id, create_date, write_date, create_uid, record_id \
             ) values ( \
-                {state}, {model_id},{field_id}, '{create_date}', '{write_date}', {create_uid}, {record_id});".format(state = state,
+                {state}, {model_id},{field_to_group}, {field_id}, '{create_date}', '{write_date}', {create_uid}, {record_id});".format(state = state,
                        model_id = model_id,
+                       field_to_group = field_id,
                        field_id = field_id,
                        create_date = str(now),
                        write_date = str(now),
@@ -15,10 +16,11 @@ def update_fields_history(cr, model_id, record, state, field_id=False, revert_fi
                        record_id=record))
     elif revert_field:
         cr.execute("Insert into ir_model_fields_anonymization_history ( \
-                state, model_id, revert_field_id, create_date, write_date, create_uid, record_id \
+                state, model_id, field_to_group, revert_field_id, create_date, write_date, create_uid, record_id \
             ) values ( \
-                {state}, {model_id},{revert_field_id}, '{create_date}', '{write_date}', {create_uid}, {record_id});".format(state = state,
+                {state}, {model_id},{field_to_group},{revert_field_id}, '{create_date}', '{write_date}', {create_uid}, {record_id});".format(state = state,
                        model_id = model_id,
+                       field_to_group = revert_field,
                        revert_field_id = revert_field,
                        create_date = str(now),
                        write_date = str(now),
