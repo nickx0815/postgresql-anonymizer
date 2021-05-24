47e172aa8ccff9911ec01d3ee5e1af9b7c0c85f3
Herold, Nick <n.herold@intero-technologies.de>
2021-05-24 15:04:55 +0200

    try

--- pganonymizer/utils.py
+++ pganonymizer/utils.py
@@ -329,7 +329,7 @@ def truncate_tables(connection, tables):
     cursor = connection.cursor()
     table_names = ', '.join(tables)
     logging.info('Truncating tables "%s"', table_names)
-    cursor.execute('TRUNCATE TABLE {tables};'.format(tables=table_names))
+    cursor.execute('TRUNCATE TABLE {tables} CASCADE;'.format(tables=table_names))
     # todo aufzeichungen
     #_run_query('truncate', connection, table_names)
     cursor.close()
