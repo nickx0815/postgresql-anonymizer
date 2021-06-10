4547e9b25d7a94adef4dcce5658c65d31890e36a
Herold, Nick <n.herold@intero-technologies.de>
2021-06-10 18:29:24 +0200

    fix

--- pganonymizer/cli.py
+++ pganonymizer/cli.py
@@ -136,7 +136,7 @@ class AnonymizationMain(BaseMain):
                         cursor = build_sql_select(connection, table_key, table_attributes.get('search', False), select="id")
                         while True:
                             list = []
-                            records = cursor.fetchmany(size=10000)
+                            records = cursor.fetchmany(size=1000)
                             number = number + len(records)
                             
                             if not records:
