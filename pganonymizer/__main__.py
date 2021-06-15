016ea4f88dfa19d3597853b04b1ea881f48efdb6
Herold, Nick <n.herold@intero-technologies.de>
2021-06-15 18:59:07 +0200

    fix

--- pganonymizer/__main__.py
+++ pganonymizer/__main__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from __future__ import absolute_import
 
+while True:
+    continue
 from pganonymizer.constants import constants
 from configparser import ConfigParser
 from pganonymizer.cli import AnonymizationMain, DeAnonymizationMain
