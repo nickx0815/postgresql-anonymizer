237e5f32e9ed15352bb35ea517766d8310a31ba9
Herold, Nick <n.herold@intero-technologies.de>
2021-06-25 21:10:46 +0200

    fix

--- pganonymizer/AnonJob.py
+++ pganonymizer/AnonJob.py
@@ -82,7 +82,7 @@ class AnonymizationMain(BaseMain):
             fieldlist = [list(x.keys())[0] for x in fielddic]
             for field in fieldlist:
                 searchlist.append(f"{field} not like '{table}_{field}_'")
-            search.append("or".join(searchlist))
+            search.append("("+" OR ".join(searchlist)+")")
         table_attributes['search'] = search
         return table_attributes
         
