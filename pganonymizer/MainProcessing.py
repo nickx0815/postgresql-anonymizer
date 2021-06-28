a1b2f07ab51ab94d1e2e3ce740ffc5fd059481d9
Herold, Nick <n.herold@intero-technologies.de>
2021-06-28 21:17:52 +0200

    fix

--- pganonymizer/MainProcessing.py
+++ pganonymizer/MainProcessing.py
@@ -20,9 +20,9 @@ from pganonymizer.constants import constants
 from pganonymizer.exceptions import BadDataFormat
 from pganonymizer.providers import get_provider
 from pganonymizer.utils import _get_ids_sql_format, _, get_table_count, build_sql_select, update_fields_history, get_connection
-from pganonymizer.logging import logger
 
 class MainProcessing():
+    logger = False
     endtime = False
     successfullrecords = 0
     successfullfields = 0
