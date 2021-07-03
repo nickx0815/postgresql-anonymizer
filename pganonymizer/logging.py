40b6cdf565fcc09a780e5255365afb57dd9c21f2
Herold, Nick <n.herold@intero-technologies.de>
2021-07-03 16:34:11 +0200

    fix

--- pganonymizer/logging.py
+++ pganonymizer/logging.py
@@ -31,8 +31,8 @@ class logger():
     def TEST_CONNECTION(self, function):
         def test_connection(self):
             result = function(self)
-            self.logging_.debug(f'the connection was set up successfully')
-            self.logging_.debug(f'connection data {self.pg_args}')
+            self.logging_.logging_.debug(f'the connection was set up successfully')
+            self.logging_.logging_.debug(f'connection data {self.pg_args}')
             return result
         return test_connection
     
@@ -42,24 +42,24 @@ class logger():
             if args.force_path_schema:
                 self.logging_.logging_.debug(f"the default schema path was forced to {args.force_path_schema}")
             result = function(self)
-            logging_.logging_.debug(f'the schema was loaded successfully')
-            logging_.logging_.debug(f'schema data {self.schema}')
+            self.logging_.logging_.debug(f'the schema was loaded successfully')
+            self.logging_.logging_.debug(f'schema data {self.schema}')
             return result
         return get_schema
     
     def NUMBER_THREAD(self, function):
         def get_thread_number(self):
             result = function(self)
-            self.logging.debug(f"Number of threads created: {result}")
+            self.logging_.logging_.debug(f"Number of threads created: {result}")
             return result
         return get_thread_number
     
     def THREAD_STARTED(self, function):
         def start_thread(self, job):
-            self.logging.debug(f"Thread started")
-            self.logging.debug(f'job\'s data {job}')
+            self.logging_.logging_.debug(f"Thread started")
+            self.logging_.logging_.debug(f'job\'s data {job}')
             result = function(self, job)
-            self.logging.debug(f"Thread finished")
+            self.logging_.logging_.debug(f"Thread finished")
             return result
         return start_thread
     
@@ -70,16 +70,16 @@ class logger():
             main = f"the {self.type_to_method_mapper(self.type)} of {self.table} took {runtime}\n"
             additionalrecordsinfo = f"successfull processed {self.successfullrecords} (total records {self.totalrecords})\n"
             additionalfieldsinfo = f"successfull processed {self.successfullfields} fields\n"
-            self.logging.debug(main)
-            self.logging.debug(additionalrecordsinfo)
-            self.logging.debug(additionalfieldsinfo)
+            self.logging_.logging_.debug(main)
+            self.logging_.logging_.logging_ing.debug(additionalrecordsinfo)
+            self.logging_.logging_.debug(additionalfieldsinfo)
             return result
         return start
     
     def ANONYMIZATION_RECORD(self, function):
         def import_data(self, connection, field, source_table, row_id, primary_key, value):
             result = function(self, connection, field, source_table, row_id, primary_key, value)
-            self.logging.info(f'{source_table} {row_id} {field} anonymized -> {value}')
+            self.logging_.logging_.info(f'{source_table} {row_id} {field} anonymized -> {value}')
             return result
         return import_data
     
@@ -87,21 +87,21 @@ class logger():
         def row_matches_excludes(self, row, excludes=None):
             result = function(self, row, excludes=excludes)
             if result:
-                self.logger.debug(f'{self.table} {row.get("id")} excluded')
+                self.logging_.logging_.debug(f'{self.table} {row.get("id")} excluded')
             return result
         return row_matches_excludes
     
     def TRUNCATE_TABLES(self, function):
         def truncate_tables(self, connection):
             result = function(self, connection)
-            self.logging.info(f'{self.schema} deleted')
+            self.logging_.logging_.info(f'{self.schema} deleted')
             return result
         return truncate_tables
     
     def INSERT_MIGRATED_FIELD(self, function):
         def insert_migrated_fields_rec(self, cr, field, table):
             result = function(self, cr, field, table)
-            self.logging.debug(f'INSERT INTO {table} ({field})')
+            self.logging_.logging_.debug(f'INSERT INTO {table} ({field})')
             return result
         return insert_migrated_fields_rec
     
@@ -112,7 +112,7 @@ class logger():
             field = list(data.keys())[0]
             id = list(data.get(field).keys())[0]
             value = data.get(field).get(id)
-            self.logging.info(f'{migrated_table} new({field} {id} {value})')
+            self.logging_.logging_.info(f'{migrated_table} new({field} {id} {value})')
             return result
         return create_anon
     
@@ -120,14 +120,14 @@ class logger():
         def update_migrated_data_history(self, cr, id, table):
             result = function(self, cr, id, table)
             migrated_table = constants.TABLE_MIGRATED_DATA+"_"+table
-            self.logging.debug(f'UPDATE {migrated_table} (state:1, id:{id})')
+            self.logging_.logging_.debug(f'UPDATE {migrated_table} (state:1, id:{id})')
             return result
         return update_migrated_data_history
     
     def DEANONYMIZATION_RECORD(self, function):
         def revert_anonymization(self, connection, record, table, field, value):
             result = function(self, connection, record, table, field, value)
-            self.logging.info(f'{table} {record[0]} {field} deanonymized -> {value}')
+            self.logging_.logging_.info(f'{table} {record[0]} {field} deanonymized -> {value}')
             return result
         return revert_anonymization
     
