ecb25f4ccaa8bb14003d6644224dfbed9269ca6a
Herold, Nick <n.herold@intero-technologies.de>
2021-07-03 16:59:11 +0200

    fix

--- tests/test_anon_processing.py
+++ tests/test_anon_processing.py
@@ -1,20 +1,85 @@
 import unittest
+from pganonymizer.args import Args
+from pganonymizer.MainJob import BaseMain
+from pganonymizer.AnonProcessing import AnonProcessing
 
 class TestAnonProcessing(unittest.TestCase):
-    def test_get_rel_method(self):
-        return
+    path = __file__.replace('test_anon_processing.py', 'utils/sample_schema_anon_processing_test.yml')
+    
+    def test_get_rel_method_tables(self):
+        args = Args({'force_path_schema':self.path,
+                     'FORCE_ANON_NUMBER_FIELD_PER_THREAD': 10})
+        testmain = BaseMain(args)
+        testprocess = AnonProcessing(testmain, "tables", 0, False, False, False)
+        self.assertEqual(testprocess._get_rel_method(), "anonymize_tables")
+    
+    def test_get_rel_method_truncate(self):
+        args = Args({'force_path_schema':self.path,
+                     'FORCE_ANON_NUMBER_FIELD_PER_THREAD': 10})
+        testmain = BaseMain(args)
+        testprocess = AnonProcessing(testmain, "truncate", 0, False, False, False)
+        self.assertEqual(testprocess._get_rel_method(), "truncate_tables")
     
     def test_get_column_dict(self):
-        return
+        args = Args({'force_path_schema':self.path,
+                     'FORCE_ANON_NUMBER_FIELD_PER_THREAD': 10})
+        testmain = BaseMain(args)
+        testprocess = AnonProcessing(testmain, "tables", 0, False, False, False)
+        column_dict = testprocess.get_column_dict(testmain.schema['tables'][0]['table1']['fields'])
+        for field in ['first_name', 'last_name', 'email']:
+            column_dict[field]=None
     
     def test_get_column_values(self):
-        return
+        args = Args({'force_path_schema':self.path,
+                     'FORCE_ANON_NUMBER_FIELD_PER_THREAD': 10})
+        testrow = {'first_name':'Testname','last_name':'Testnachname','email':'test@test.de'}
+        row_info = {'table':'table1','id':1}
+        testmain = BaseMain(args)
+        testprocess = AnonProcessing(testmain, "tables", 0, False, False, False)
+        column_dict = testprocess.get_column_values(testrow,
+                                                    testmain.schema['tables'][0]['table1']['fields'],
+                                                    row_info)
+        self.assertTrue(testrow['first_name'] != column_dict['first_name'])
+        self.assertTrue(testrow['last_name'] != column_dict['last_name'])
+        self.assertTrue(testrow['email'] != column_dict['email'])
     
-    def test_get_anon_field_id(self):
-        return
+    def test_row_matches_excludes_true(self):
+        args = Args({'force_path_schema':self.path,
+                     'FORCE_ANON_NUMBER_FIELD_PER_THREAD': 10})
+        testrow = {'first_name':'Testname','last_name':'Testnachname','email':'test@example.com'}
+        testmain = BaseMain(args)
+        testprocess = AnonProcessing(testmain, "tables", 0, False, False, False)
+        result = testprocess.row_matches_excludes(testrow, excludes=testmain.schema['tables'][0]['table1']['excludes'])
+        self.assertTrue(result)
     
-    def test_row_matches_excludes(self):
-        return
+    def test_row_matches_excludes_false(self):
+        args = Args({'force_path_schema':self.path,
+                     'FORCE_ANON_NUMBER_FIELD_PER_THREAD': 10})
+        testrow = {'first_name':'Testname','last_name':'Testnachname','email':'test@test.com'}
+        testmain = BaseMain(args)
+        testprocess = AnonProcessing(testmain, "tables", 0, False, False, False)
+        result = testprocess.row_matches_excludes(testrow, excludes=testmain.schema['tables'][0]['table1']['excludes'])
+        self.assertFalse(result)
+    
+    def test_row_matches_excludes_id_true(self):
+        args = Args({'force_path_schema':self.path,
+                     'FORCE_ANON_NUMBER_FIELD_PER_THREAD': 10})
+        testrow = {'id':10, 'first_name':'Testname','last_name':'Testnachname','email':'test@example.com'}
+        testmain = BaseMain(args)
+        testprocess = AnonProcessing(testmain, "tables", 0, False, False, False)
+        result = testprocess.row_matches_excludes(testrow, excludes=testmain.schema['tables'][1]['table2']['excludes'])
+        self.assertTrue(result)
+    
+    def test_row_matches_excludes__id_false(self):
+        args = Args({'force_path_schema':self.path,
+                     'FORCE_ANON_NUMBER_FIELD_PER_THREAD': 10})
+        testrow = {'id':2, 'first_name':'Testname','last_name':'Testnachname','email':'test@test.com'}
+        testmain = BaseMain(args)
+        testprocess = AnonProcessing(testmain, "tables", 0, False, False, False)
+        result = testprocess.row_matches_excludes(testrow, excludes=testmain.schema['tables'][1]['table2']['excludes'])
+        self.assertFalse(result)
     
     def test_exclude_eval(self):
         return
+    
+    
