933f0e3dfd32e1c8af1ee7aaa02bb0b479c9bbf9
Herold, Nick <n.herold@intero-technologies.de>
2021-07-05 17:54:57 +0200

    fix

--- pganonymizer/utils.py
+++ pganonymizer/utils.py
@@ -92,7 +92,7 @@ def _get_mapped_data(con, table, fields):
     list = []
     for field in fields:
         #field_parsed = _get_ids_sql_format(fields, char=True)
-        select_model_id_sql = f"SELECT old_model_name, new_model_name, old_field_name, new_field_name FROM {constants.TABLE_MIGRATED_DATA_MAPPING} where old_model_name = '{table}' and old_field_name in {field}"
+        select_model_id_sql = f"SELECT old_model_name, new_model_name, old_field_name, new_field_name FROM {constants.TABLE_MIGRATED_DATA_MAPPING} where old_model_name = '{table}' and old_field_name = '{field}'"
         cr.execute(select_model_id_sql)
         while True:
             records = cr.fetchone()
